582b0d4675efb1e410816698532430beb0a70778
Rachel Lin <rachellin443@gmail.com>
2022-07-25 14:57:33 -0400

    Set up game loop with game over alert after 5 wins

--- javascript.js
+++ javascript.js
@@ -6,6 +6,10 @@
 - game() func that runs playRound() in a loop from 0 to 5
 */
 
+let playerScore = 0;
+let computerScore = 0;
+const WINS = 5;
+
 function computerPlay() {
     let random = Math.floor(Math.random() * 3);
     switch (random) {
@@ -21,6 +25,16 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+
+    if (playerScore == WINS) {
+        alert("GAME OVER: PLAYER WINS");
+        return;
+    }
+    else if (computerScore == WINS) {
+        alert("GAME OVER: COMPUTER WINS");
+        return;
+    }
+
     console.log(playerSelection);
     console.log(computerSelection);
     if (playerSelection == "scissors" && computerSelection == "scissors"
@@ -32,19 +46,32 @@ function playRound(playerSelection, computerSelection) {
     || playerSelection == "rock" && computerSelection == "paper"
     || playerSelection == "scissors" && computerSelection == "rock") {
         console.log("comp win");
+        computerScore++;
     }
     else if (playerSelection == "rock" && computerSelection == "scissors"
     || playerSelection == "scissors" && computerSelection == "paper"
     || playerSelection == "paper" && computerSelection == "rock") {
         console.log("player win!!!!!!");    
+        playerScore++;
     }
+    console.log(playerScore);
+    console.log(computerScore);
 }
 
-function game(id) {
-    let userChoice = id;
+
+
+window.onload = function() {
+    playerScore = 0;
+    computerScore = 0;
+}
+
+function game(id, playerScore, computerScore) {
+    userChoice= id;
     playRound(userChoice, computerPlay());
 
+
+
 }
 
 
-console.log(computerPlay());
+//console.log(computerPlay());
